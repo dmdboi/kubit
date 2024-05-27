75a51b03d1e9047c3bb6256b9de596411b4b1c8c
dmdboi <padfootdiamond@gmail.com>
2024-05-27 18:07:28 +0100

    chore: delete sentry package

--- packages/sentry/adonis-typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@ioc:Kubit/Sentry' {
-  import type { NodeOptions } from '@sentry/node';
-  export interface SentryConfig extends Omit<NodeOptions, 'enabled'> {}
-  import * as Sentry from '@sentry/node';
-
-  export default Sentry;
-}
